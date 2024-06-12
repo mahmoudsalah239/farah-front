b2addaf51e33e69db1e7cf1df6ac3d8d9cc98817
nesmafayz <nesmafayez21@gmail.com>
2024-06-13 02:12:56 +0300

    commit hall

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
-
 import { AccountComponent } from '../Auth/account/account.component';
 import { RegisterComponent } from '../Auth/register/register.component';
 import { LoginComponent } from '../Auth/login/login.component';
@@ -10,16 +9,17 @@ import { PhotographerComponent } from '../components/photographer/photographer.c
 import { DressComponent } from '../components/dress/dress.component';
 import { CarComponent } from '../components/car/car.component';
 import { BeautyCenterComponent } from '../components/beauty-center/beauty-center.component';
+import { HallDetailsComponent } from '../components/hall/hall-details/hall-details.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'hall', component: HallComponent },
+  {path:'hall-details/id:',component:HallDetailsComponent},
   { path: 'photographer', component: PhotographerComponent },
   { path: 'dress', component: DressComponent },
   { path: 'car', component: CarComponent },
   { path: 'beauty', component: BeautyCenterComponent },
-
   { path: 'Account', component: AccountComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'Login', component: LoginComponent },
--- src/components/hall/hall-details/hall-details.component.ts
+++ src/components/hall/hall-details/hall-details.component.ts
@@ -1,17 +1,25 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hall-details',
   standalone: true,
   imports: [CarouselModule, CommonModule],
   templateUrl: './hall-details.component.html',
-  styleUrl: './hall-details.component.scss',
+  styleUrls: ['./hall-details.component.scss'],
 })
 export class HallDetailsComponent {
-  images: string[];
-  constructor() {
-    this.images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+  images: string[] = []; 
+  hallName: string = ''; 
+  price: number = 0; 
+  capacity: number = 0; 
+
+  constructor(private route: ActivatedRoute) {
+    this.images = ['image1.jpg', 'image2.jpg', 'image3.jpg']; 
+    this.hallName = 'قاعة الفخامة'; 
+    this.price = 7500.00;
+    this.capacity = 300; 
   }
 }
