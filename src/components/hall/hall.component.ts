f6f26d048d294bb560d8c1286383b404b8522897
nesmafayz <nesmafayez21@gmail.com>
2024-06-19 20:43:01 +0300

    filter

--- src/components/hall/hall.component.ts
+++ src/components/hall/hall.component.ts
@@ -1,34 +1,61 @@
+
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hall',
-  standalone:true,
+  standalone: true,
   templateUrl: './hall.component.html',
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   styleUrls: ['./hall.component.scss']
 })
 export class HallComponent implements OnInit {
   halls = [
-    { id: 1, imageUrl: 'https://www.arabiaweddings.com/sites/default/files/styles/max980/public/listing/2023/08/10/royal_plaza.jpg?itok=pnzy7apt', price: 'السعر', name: 'اسم القاعة', description: 'الوصف' },
-    { id: 2, imageUrl: 'https://www.ramstarab.com/wp-content/uploads/2021/12/%D8%B5%D9%88%D8%B1%D8%A9-%D9%81%D8%B1%D8%AD-17.jpg', price: 'السعر', name: 'اسم القاعة', description: 'الوصف' },
-    { id: 3, imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgFw7SnZQ7AEx4zBwTjCM-ALnLihCUHaomWLTP9X_tF5JtjyTImG7jlPcFwTW4v8WwzBT1kHNYRTBHwJLoRu53CNF73mS_Ptus1eJ_20j9zkJ5thoVE15XYAd73NHdiP3NsictAOB3Z73bNU1s2VdD8RrGmxnQB7w05_bUxRg8OJz4EaLfRz2yMETzqip8e/s1668/318627933_704502801026406_2918971883659758944_n.jpg', price: 'السعر', name: 'اسم القاعة', description: 'الوصف' },
-    { id: 4, imageUrl: 'https://ongineering.com/images/Articles_Aziz/wedding-hall-decoration-design.jpg', price: 'السعر',  name: 'اسم القاعة',description: 'الوصف' }
+    { id: 1, imageUrl: 'https://www.arabiaweddings.com/sites/default/files/styles/max980/public/listing/2023/08/10/royal_plaza.jpg?itok=pnzy7apt', price: 1500, name: 'قاعة 1', description: 'الوصف 1', city: 'القاهرة', town: 'القاهرة' },
+    { id: 2, imageUrl: 'https://www.ramstarab.com/wp-content/uploads/2021/12/%D8%B5%D9%88%D8%B1%D8%A9-%D9%81%D8%B1%D8%AD-17.jpg', price: 3000, name: 'قاعة 2', description: 'الوصف 2', city: 'الإسكندرية', town: 'الإسكندرية' },
+    { id: 3, imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgFw7SnZQ7AEx4zBwTjCM-ALnLihCUHaomWLTP9X_tF5JtjyTImG7jlPcFwTW4v8WwzBT1kHNYRTBHwJLoRu53CNF73mS_Ptus1eJ_20j9zkJ5thoVE15XYAd73NHdiP3NsictAOB3Z73bNU1s2VdD8RrGmxnQB7w05_bUxRg8OJz4EaLfRz2yMETzqip8e/s1668/318627933_704502801026406_2918971883659758944_n.jpg', price: 4500, name: 'قاعة 3', description: 'الوصف 3', city: 'الجيزة', town: 'الجيزة' },
+    { id: 4, imageUrl: 'https://ongineering.com/images/Articles_Aziz/wedding-hall-decoration-design.jpg', price: 5000, name: 'قاعة 4', description: 'الوصف 4', city: 'بورسعيد', town: 'بورسعيد' }
   ];
 
   // Pagination variables
   currentPage: number = 1;
-  itemsPerPage: number = 16; 
+  itemsPerPage: number = 16;
+  selectedCity: string = '';
+  selectedTown: string = '';
+  maxPrice: number = 5000;
+
+  towns: string[] = [
+    'القاهرة',
+    'الإسكندرية',
+    'الجيزة',
+    'بورسعيد'
+  ];
+
+  cities: string[] = [
+    'القاهرة',
+    'الإسكندرية',
+    'الجيزة',
+    'بورسعيد'
+  ];
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    
+    this.filterHalls();
   }
 
-  navigateToDetails(hallId: number): void {
-    this.router.navigate(['/hall-details', hallId]);
+  get filteredHalls() {
+    return this.halls.filter(hall => {
+      return (!this.selectedTown || hall.town === this.selectedTown)
+        && (!this.selectedCity || hall.city === this.selectedCity)
+        && (hall.price <= this.maxPrice);
+    });
+  }
+
+  filterHalls() {
+    this.currentPage = 1;  // Reset pagination to the first page after filtering
   }
 
   // Pagination methods
@@ -38,10 +65,9 @@ export class HallComponent implements OnInit {
 
   getPaginatedHalls(): any[] {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    return this.halls.slice(startIndex, startIndex + this.itemsPerPage);
+    return this.filteredHalls.slice(startIndex, startIndex + this.itemsPerPage);
   }
 
-  
   truncateDescription(description: string): string {
     const words = description.split(' ');
     if (words.length > 10) {
