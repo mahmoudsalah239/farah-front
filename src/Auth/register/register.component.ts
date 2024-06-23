2251b2251e563ed1de61329153e05749f1af80e1
moudy99 <bobmoudy1@gmail.com>
2024-06-24 01:40:26 +0300

    add customer register

--- src/Auth/register/register.component.ts
+++ src/Auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+
 import {
   AbstractControl,
   FormBuilder,
@@ -16,6 +17,7 @@ import { City } from '../../interfaces/city';
 import { RegisterService } from '../../services/register.service';
 import { mustMatch } from '../../interfaces/validators';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { SendOtpService } from '../../services/send-otp.service';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -31,8 +33,11 @@ export class RegisterComponent {
   selectedGovernorate: Governorate | null = null;
 
   constructor(
+    private router: Router,
+
     private addressService: AddressServiceService,
-    private _registerService: RegisterService,
+    private registerService: RegisterService,
+    private sendOtpService: SendOtpService,
     private spinner: NgxSpinnerService
   ) {
     this.registerForm = new FormGroup(
@@ -41,10 +46,13 @@ export class RegisterComponent {
         lastName: new FormControl('', Validators.required),
         ssn: new FormControl('', [
           Validators.required,
-          Validators.pattern(/^\d{14}$/),
+          Validators.pattern(/^\d{5}$/),
         ]),
         email: new FormControl('', [Validators.required, Validators.email]),
-        phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^01[0-2]{1}[0-9]{8}$')]),
+        phoneNumber: new FormControl('', [
+          Validators.required,
+          Validators.pattern('^01[0-2]{1}[0-9]{8}$'),
+        ]),
         govID: new FormControl('', Validators.required),
         cityID: new FormControl('', Validators.required),
         password: new FormControl('', [
@@ -89,26 +97,86 @@ export class RegisterComponent {
   }
   onSubmit() {
     if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
-      this._registerService.register(this.registerForm.value).subscribe({
+      this.spinner.show();
+      this.registerService.register(this.registerForm.value).subscribe({
         next: (res) => {
-          Swal.fire({
-            title: "Good job!",
-            text: "You clicked the button!",
-            icon: "success"
-          });
-          console.log(res);
+          this.spinner.hide();
+          localStorage.setItem('token', res.body.data.token);
+          this.promptForOtp();
         },
         error: (err) => {
-         Swal.fire({
-  icon: "error",
-  title: "Oops...",
-  text: "Something went wrong!",
-  footer: '<a href="#">Why do I have this issue?</a>'
-});
+          this.spinner.hide();
+          Swal.fire({
+            icon: 'error',
+            title: 'عذرًا...',
+            text: 'حدث خطأ ما!',
+            footer: '<a href="#">لماذا أواجه هذه المشكلة؟</a>',
+          });
           console.log(err);
         },
       });
     }
+  }
+
+  promptForOtp() {
+    Swal.fire({
+      icon: 'info',
+      title: 'تأكيد OTP',
+      text: 'تم إرسال OTP إلى بريدك الإلكتروني. الرجاء إدخاله أدناه:',
+      input: 'text',
+      inputPlaceholder: 'أدخل OTP',
+      showCancelButton: true,
+      confirmButtonText: 'تحقق',
+      cancelButtonText: 'إلغاء',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const otp = result.value;
+        this.verifyOtp(otp);
+      }
+    });
+  }
+
+  verifyOtp(otp: string) {
+    this.spinner.show();
+    this.sendOtpService.confirmEmail(otp).subscribe({
+      next: (verifyResponse) => {
+        this.spinner.hide();
+        if (verifyResponse && verifyResponse.succeeded) {
+          Swal.fire({
+            icon: 'success',
+            title: 'تم التحقق من البريد الإلكتروني',
+            text: 'تم التحقق من بريدك الإلكتروني بنجاح. يمكنك الآن تسجيل الدخول.',
+          }).then(() => {
+            this.router.navigate(['/Login']);
+          });
+        } else {
+          this.promptForOtpWithMessage('OTP غير صالح أو منتهي. حاول مرة أخرى.');
+        }
+      },
+      error: (error) => {
+        this.spinner.hide();
+        this.promptForOtpWithMessage(
+          'حدث خطأ أثناء التحقق من OTP. حاول مرة أخرى.'
+        );
+      },
+    });
+  }
+
+  promptForOtpWithMessage(message: string) {
+    Swal.fire({
+      icon: 'info',
+      title: 'تأكيد OTP',
+      text: message,
+      input: 'text',
+      inputPlaceholder: 'أدخل OTP',
+      showCancelButton: true,
+      confirmButtonText: 'تحقق',
+      cancelButtonText: 'إلغاء',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const otp = result.value;
+        this.verifyOtp(otp);
+      }
+    });
   }
 }
