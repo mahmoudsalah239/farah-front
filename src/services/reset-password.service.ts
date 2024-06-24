ca6d8ec078faa6d5b1f9f5d008a74dadc97c574c
moudy99 <bobmoudy1@gmail.com>
2024-06-24 14:44:02 +0300

    add login

---
+++ src/services/reset-password.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.development';
+@Injectable({
+  providedIn: 'root',
+})
+export class ResetPasswordService {
+  private apiUrl = `${environment.apiUrl}/Account`;
+
+  constructor(private http: HttpClient) {}
+
+  forgetPassword(email: string): Observable<any> {
+    const url = `${this.apiUrl}/forgetPassword?Email=${email}`;
+    return this.http.post<any>(url, null);
+  }
+
+  changePassword(
+    oldPassword: string,
+    newPassword: string,
+    securityQuestionAnswer: string
+  ): Observable<any> {
+    const url = `${this.apiUrl}/changePassword`;
+    const token =
+      localStorage.getItem('token') || sessionStorage.getItem('token');
+    if (!token) {
+      throw new Error('Token not found in localStorage or sessionStorage');
+    }
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    const body = {
+      oldPassword,
+      newPassword,
+      securityQuestionAnswer,
+    };
+    return this.http.post<any>(url, body, { headers });
+  }
+}
