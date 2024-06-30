06545c97818055247e25c28cb94f6547f53ab846
nesmafayz <nesmafayez21@gmail.com>
2024-06-30 21:43:04 +0300

    favorites

--- src/components/favorites/favorites.component.ts
+++ src/components/favorites/favorites.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.scss'
 })
 export class FavoritesComponent {
- 
-
   
+
 }
